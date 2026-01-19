8c2618a09f2f5185d89883632010d1547cda0ccc
saijaku0 <minerking112256@gmail.com>
2026-01-19 13:13:04 +0100

    refactor: remove ssr

--- app/booking-ui/src/app/app.config.ts
+++ app/booking-ui/src/app/app.config.ts
@@ -2,14 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient(),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
 };
