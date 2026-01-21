8e81439012fb0b2255a45389b99ba5529b3b230c
saijaku0 <minerking112256@gmail.com>
2026-01-21 17:05:42 +0100

    feat: add header style

--- app/booking-ui/src/header/header.ts
+++ app/booking-ui/src/header/header.ts
@@ -1,5 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { BasicComponentInjection } from '../app/component-injection.service';
+
+interface Specialties {
+  id: number;
+  name: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-header',
@@ -8,5 +14,18 @@ import { BasicComponentInjection } from '../app/component-injection.service';
   styleUrl: './header.scss',
 })
 export class Header {
+  menuOpen = signal(false);
+  specialtiesOptions = signal<Specialties[]>([
+    { id: 1, name: 'Cardiology', link: '/specialties/cardiology' },
+    { id: 2, name: 'Dermatology', link: '/specialties/dermatology' },
+    { id: 3, name: 'Neurology', link: '/specialties/neurology' },
+    { id: 4, name: 'Pediatrics', link: '/specialties/pediatrics' },
+    { id: 5, name: 'Psychiatry', link: '/specialties/psychiatry' },
+    { id: 6, name: 'Radiology', link: '/specialties/radiology' },
+  ]);
   private componentInjection = inject(BasicComponentInjection);
+
+  toggleMenu() {
+    this.menuOpen.set(!this.menuOpen());
+  }
 }
