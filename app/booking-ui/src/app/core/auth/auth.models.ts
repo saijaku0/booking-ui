768e91a7e2a3b55bafa1882b5518ccbf54dffb85
saijaku0 <minerking112256@gmail.com>
2026-02-10 10:50:21 +0100

    feat: add model

--- app/booking-ui/src/app/core/auth/auth.models.ts
+++ app/booking-ui/src/app/core/auth/auth.models.ts
@@ -26,12 +26,12 @@ export interface CurrentUser {
   roles: UserRole[];
 }
 
-export interface JwtPayload {
-  sub?: string;
-  email?: string;
+export interface JwtCustomPayload {
   role?: string | string[];
-  exp?: number;
-  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'?: string;
-  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'?: string;
   'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'?: string | string[];
+  sub?: string;
+  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'?: string;
+  email?: string;
+  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'?: string;
+  exp?: number;
 }
