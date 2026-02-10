5e6edd77bf91b2fb1ca5c9b05a61ce6cbaec8a5e
saijaku0 <minerking112256@gmail.com>
2026-02-10 10:49:39 +0100

    feat: add routes for login

--- app/booking-ui/src/app/app.routes.ts
+++ app/booking-ui/src/app/app.routes.ts
@@ -1,4 +1,20 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { DoctorLayout } from './doctor/layout/doctor-layout/doctor-layout';
+import { Dashboard } from './doctor/pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  {
+    path: 'doctor',
+    component: DoctorLayout,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: Dashboard },
+    ],
+  },
+
+  { path: '**', redirectTo: '' },
+];
