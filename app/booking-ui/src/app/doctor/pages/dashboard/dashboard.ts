4b66c4a7a4229e33318571ea2bc51a179f51edb5
saijaku0 <minerking112256@gmail.com>
2026-02-11 12:16:45 +0100

    fix: doctor dashboard

--- app/booking-ui/src/app/doctor/pages/dashboard/dashboard.ts
+++ app/booking-ui/src/app/doctor/pages/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { AppointmentService, DoctorService } from '@core/services/index';
 import { AppointmentDto, AppointmentStatus } from '@core/models/appointmnet.models';
 
@@ -23,6 +23,14 @@ export class Dashboard {
     earnings: 0,
   };
 
+  doctorFullName = computed(() => {
+    const doc = this.doctorService.currentDoctor();
+    if (doc) {
+      return `${doc.name} ${doc.lastname}`;
+    }
+    return 'Doctor';
+  });
+
   constructor() {
     effect(() => {
       const doctorId = this.doctorService.currentDoctorId();
@@ -31,6 +39,44 @@ export class Dashboard {
         this.loadSchedule(doctorId);
       }
     });
+  }
+
+  getInitials(name: string): string {
+    if (!name) return '';
+    const parts = name.split(' ');
+    if (parts.length === 1) return parts[0].substring(0, 2).toUpperCase();
+    return (parts[0][0] + parts[1][0]).toUpperCase();
+  }
+
+  generateColor(name: string | undefined): string {
+    if (!name) return '#e0e0e0';
+
+    const colors = [
+      '#FFCDD2',
+      '#F8BBD0',
+      '#E1BEE7',
+      '#D1C4E9',
+      '#C5CAE9',
+      '#BBDEFB',
+      '#B3E5FC',
+      '#B2EBF2',
+      '#B2DFDB',
+      '#C8E6C9',
+      '#DCEDC8',
+      '#F0F4C3',
+      '#FFECB3',
+      '#FFE0B2',
+      '#FFCCBC',
+    ];
+
+    let hash = 0;
+    for (let i = 0; i < name.length; i++) {
+      hash = name.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    const index = Math.abs(hash % colors.length);
+
+    return colors[index];
   }
 
   loadSchedule(doctorId: string) {
